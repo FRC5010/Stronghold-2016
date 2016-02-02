52024b290bd5ded26e34df82d13f9dad3722869c
sschrisman <steve.chrisman@oneamerica.com>
2016-02-02 18:06:19 -0500

    Added code for CamTilt and CastleScaling

--- Stronghold/src/org/usfirst/frc/team5010/scalingArm/Camtilt.java
+++ Stronghold/src/org/usfirst/frc/team5010/scalingArm/Camtilt.java
@@ -1,5 +1,24 @@
 package org.usfirst.frc.team5010.scalingArm;
 
-public class Camtilt {
+import org.usfirst.frc.team5010.oi.JoystickManager;
+import org.usfirst.frc.team5010.robot.LogicManager;
 
+public class Camtilt implements LogicManager {
+	private JoystickManager joystickManager;
+	public Camtilt(JoystickManager joystickMgr) {
+		joystickManager = joystickMgr;
+		
+	
+	}
+	@Override
+	public void update() {
+		if (joystickManager.moveCamtiltIn()) {
+		// TODO write code to moveCamtilt else 	
+		
+		} else {
+			if (joystickManager.moveCamtiltOut()) {
+			//TODO write code to moveCametilt out
+			}	
+		}
+	}
 }
